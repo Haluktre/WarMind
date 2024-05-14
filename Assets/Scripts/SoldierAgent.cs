2f41d79cf6e92c6428e1c45479e2ce29a878667b
Haluk Türe <halukture0@gmail.com>
2024-05-14 14:36:06 +0300

    Added controller to SoldierAgent

--- Assets/Scripts/SoldierAgent.cs
+++ Assets/Scripts/SoldierAgent.cs
@@ -7,9 +7,15 @@ using UnityEngine;
 
 public class SoldierAgent : Agent
 {
+    SoldierController soldierController;
+
+    private void Start()
+    {
+        soldierController = GetComponent<SoldierController>();
+    }
     public override void CollectObservations(VectorSensor sensor)
     {
-        
+
     }
 
     public override void OnEpisodeBegin()
@@ -19,8 +25,55 @@ public class SoldierAgent : Agent
 
     public override void OnActionReceived(ActionBuffers actions)
     {
-        
+
+        soldierController.horizontalInput = actions.DiscreteActions[0];
+        soldierController.verticalInput = actions.DiscreteActions[1];
+        soldierController.rotateDirect = actions.DiscreteActions[2];
+        soldierController.isFire = actions.DiscreteActions[3];
+
+        soldierController.SoldierMovement();
     }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActions = actionsOut.DiscreteActions;
+
+        switch (Mathf.RoundToInt(Input.GetAxisRaw("Horizontal")))
+        {
+            case -1: discreteActions[0] = -1; break;
+            case 0: discreteActions[0] = 0; break;
+            case 1: discreteActions[0] = 1; break;
+        }
+        switch (Mathf.RoundToInt(Input.GetAxisRaw("Vertical")))
+        {
+            case -1: discreteActions[1] = -1; break;
+            case 0: discreteActions[1] = 0; break;
+            case 1: discreteActions[1] = 1; break;
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            discreteActions[2] = -1;
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            discreteActions[2] = 1;
+        }
+        else
+        {
+            discreteActions[2] = 0;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            discreteActions[3] = 1;
+        }
+        else
+        {
+            discreteActions[3] = 0;
+        }
+    }
+
 
 
 }
--- Assets/Scripts/SoldierController.cs
+++ Assets/Scripts/SoldierController.cs
@@ -9,15 +9,17 @@ public class SoldierController : MonoBehaviour
     [SerializeField] private GameObject _bullet;
     [SerializeField] private Transform _soldier;
     [SerializeField] private Transform _gun;
-    public enum MyOptions {RedBullet, BlueBullet}
+    public enum MyOptions { RedBullet, BlueBullet }
     [SerializeField] private MyOptions ChoiceEnemyBullet;
 
     private bool _isLive = true;
     public float health = 3f;
     public float speed = 2f; // Movement speed of the player
     public float rotationSpeed = 100f;
-    private float horizontalInput;
-    private float verticalInput;
+    public float horizontalInput;
+    public float verticalInput;
+    public int rotateDirect;
+    public int isFire;
 
     public float fireRate = 0.1f; // Ateş hızı (saniyede mermi sayısı)
     private float lastFireTime = 0f; // Son ateşleme zamanı
@@ -26,13 +28,10 @@ public class SoldierController : MonoBehaviour
     {
         health = 3f;
     }
+
     void Update()
     {
-        // Get horizontal and vertical input from keyboard or joystick
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
-
-        if(Input.GetKey(KeyCode.Space) )
+        if (isFire == 1)
         {
             if (Time.time - lastFireTime >= fireRate)
             {
@@ -41,22 +40,6 @@ public class SoldierController : MonoBehaviour
                 lastFireTime = Time.time;
             }
         }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            _soldier.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            _soldier.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
-        }
-    }
-
-    void FixedUpdate()
-    {
-        // Move the player based on input
-        Vector3 movement = new Vector2(horizontalInput * speed, verticalInput * speed);
-        transform.Translate(movement * Time.deltaTime);
     }
 
     private void GunFire()
@@ -64,17 +47,25 @@ public class SoldierController : MonoBehaviour
         Instantiate(_bullet, new Vector2(_gun.position.x, _gun.position.y), _gun.rotation);
     }
 
-    
+    public void SoldierMovement()
+    {
+        // Move the player based on input
+        Vector3 movement = new Vector2(horizontalInput * speed, verticalInput * speed);
+        transform.Translate(movement * Time.deltaTime);
+
+        _soldier.Rotate(0f, 0f, rotateDirect * rotationSpeed * Time.deltaTime);
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == ChoiceEnemyBullet.ToString())
         {
             health = health - 1f;
-            if(health == 0f && _isLive == true)
+            if (health == 0f && _isLive == true)
             {
                 _isLive = false;
                 Destroy(this.gameObject);
-                
+
             }
         }
     }
